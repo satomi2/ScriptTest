1d171a9ed2d06767929d47e4aa68a4dadddcdd81
satomi2 <sato.t.sin2117@gmail.com>
2017-11-05 01:15:17 +0900

    ScriptTest
    課題提出　配列を宣言して出力しよう

---
+++ ScriptTest/Assets/Test.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss {        
+	private int hp = 100;          // 体力
+	private int power = 25; // 攻撃力
+
+	// 攻撃用の関数
+	public void Attack() { 
+		Debug.Log( this.power + "のダメージを与えた" );
+	}
+
+	// 防御用の関数
+	public  void Defence(int damage) { 
+		Debug.Log( damage+"のダメージを受けた" );
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+
+}
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+		// 要素数5の配列を初期化する
+		int[] points = new int[5];
+
+		// 配列の各要素に値を代入する
+		points [0] = 10;
+		points [1] = 20;
+		points [2] = 30;
+		points [3] = 40;
+		points [4] = 50;
+
+		// 配列の要素をすべて表示する
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (points [i]);
+			// 配列の要素をすべて表示する
+		for (int i = 5; i < 0; i++) {
+			Debug.Log (points [i]);
+		}
+	}
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+
+		// 攻撃用の関数を呼び出す
+		lastboss.Attack();
+		// 防御用の関数を呼び出す
+		lastboss.Defence(3);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
